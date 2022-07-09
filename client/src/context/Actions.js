5e90ef811c0af5a3b203b16f481d28157147a21f
Aman-dev1504 <Amanmalviya786an@gmail.com>
2022-07-09 18:47:15 +0530

    .

---
+++ client/src/context/Actions.js
@@ -0,0 +1,12 @@
+export const LoginStart =(userCredentials)=>({
+    type:"LOGIN_START"
+})
+
+export const LoginSuccess=(user)=>({
+    type:"LOGIN_SUCCESS",
+    payload:user,
+});
+export const LOGINFAILURE=()=>({
+    type:"LOGIN_FAILURE",
+    
+})
